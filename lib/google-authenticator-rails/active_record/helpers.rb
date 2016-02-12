ed4fbf3ac2499348d6e749b48d7acd125aea9dc6
Wagner Caixeta <wagner@assembleia.go.gov.br>
2016-02-12 13:27:12 -0200

    Add qr_code_png and qr_code_svg methods for generating offline qr code

--- lib/google-authenticator-rails/active_record/helpers.rb
+++ lib/google-authenticator-rails/active_record/helpers.rb
@@ -14,6 +14,14 @@ module GoogleAuthenticatorRails # :nodoc:
         GoogleQR.new(:data => ROTP::TOTP.new(google_secret_value, :issuer => google_issuer).provisioning_uri(google_label.to_s), :size => "200x200").to_s
       end
 
+      def qr_code_png
+        RQRCode::QRCode.new(ROTP::TOTP.new(google_secret_value, :issuer => google_issuer).provisioning_uri(google_label.to_s), :size => 200).as_png
+      end
+
+      def qr_code_svg
+        RQRCode::QRCode.new(ROTP::TOTP.new(google_secret_value, :issuer => google_issuer).provisioning_uri(google_label.to_s), :size => 200).as_svg
+      end
+
       def google_label
         method = self.class.google_label_method
         case method
